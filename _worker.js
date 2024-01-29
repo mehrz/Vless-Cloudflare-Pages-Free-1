784229c53cd330c7e9adfb413752102cfe960997
mehrz <104155701+mehrz@users.noreply.github.com>
2024-01-29 18:52:16 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '441f7b53-0078-42d3-8f39-9790e2386cc9';
+let userID = 'e6ce7f6c-1830-4abf-b7e7-0fac323f2ade';
 
 const proxyIPs = ['23.162.136.169', 'cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
 
